bd721257be9cd5c143d64537ad7c049c0a66f7ee
marksev1 <marksev1@gmail.com>
2017-02-12 13:34:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sun skill
+
+This skill provides times for relevant sun events (sunrise, sunset, etc.).
+
+## How to install
+
+Unzip or clone `SunSkill` into `mycroft-core/mycroft/skills` directory.
+
+`pip install arrow`,`pip install solartime`,`pip install geopy` and `pip install tzlocal` into mycroft's virtual environment.
+
+Restart mycroft instance and test with the following sentences: "when does the sun set" or "when does the sun rise".
+
+Thanks to forslund and jarbasai for help.
---
+++ SunSkill/__init__.py
@@ -0,0 +1,79 @@
+from os.path import dirname
+from adapt.intent import IntentBuilder
+from mycroft.skills.core import MycroftSkill
+from mycroft.util.log import getLogger
+
+import arrow
+import tzlocal
+
+from solartime import SolarTime
+
+__author__ = 'msev'
+
+LOGGER = getLogger(__name__)
+
+from geopy.geocoders import Nominatim
+
+class SunSkill(MycroftSkill):
+    def __init__(self):
+        super(SunSkill, self).__init__(name="SunSkill")
+
+    def initialize(self):
+        self.load_data_files(dirname(__file__))
+        sunrise_intent = IntentBuilder("SunRiseIntent"). \
+            require("SunRiseKeyword").build()
+        sunset_intent = IntentBuilder("SunSetIntent"). \
+            require("SunSetKeyword").build()
+        dawn_intent = IntentBuilder("DawnIntent"). \
+            require("DawnKeyword").build()
+        dusk_intent = IntentBuilder("DuskIntent"). \
+            require("DuskKeyword").build()
+        solarnoon_intent = IntentBuilder("SolarnoonIntent"). \
+            require("SolarnoonKeyword").build()
+
+        self.register_intent(sunrise_intent, self.handle_sunrise_intent)
+        self.register_intent(sunset_intent, self.handle_sunset_intent)
+        self.register_intent(dawn_intent, self.handle_dawn_intent)
+        self.register_intent(dusk_intent, self.handle_dusk_intent)
+        self.register_intent(solarnoon_intent, self.handle_solarnoon_intent)
+
+        today = arrow.now().date()
+        self.localtz = tzlocal.get_localzone()
+
+        
+        city = self.config_core.get("location")
+        geolocator = Nominatim()
+        location = geolocator.geocode(city)
+        lat = location.latitude
+        lon = location.longitude
+        
+
+        sun = SolarTime()
+        self.schedule = sun.sun_utc(today, lat, lon)
+
+    def handle_sunrise_intent(self, message):
+        sunrise = self.schedule['sunrise'].astimezone(self.localtz)
+        self.speak_dialog("sunrise", {"sunrise": str(sunrise)[10:16]})
+
+    def handle_sunset_intent(self, message):
+        sunset = self.schedule['sunset'].astimezone(self.localtz)
+        self.speak_dialog("sunset", {"sunset": str(sunset)[10:16]})
+
+    def handle_dawn_intent(self, message):
+        dawn = self.schedule['dawn'].astimezone(self.localtz)
+        self.speak_dialog("dawn", {"dawn": str(dawn)[10:16]})
+
+    def handle_dusk_intent(self, message):
+        dusk = self.schedule['dusk'].astimezone(self.localtz)
+        self.speak_dialog("dusk", {"dusk": str(dusk)[10:16]})
+
+    def handle_solarnoon_intent(self, message):
+        solarnoon = self.schedule['noon'].astimezone(self.localtz)
+        self.speak_dialog("solarnoon", {"solarnoon": str(noon)[10:16]})
+
+    def stop(self):
+        pass
+
+
+def create_skill():
+    return SunSkill()
