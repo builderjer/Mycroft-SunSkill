2648449d2550194daa0be11f0a515e534325192d
JarbasAI <somom@30wave.com>
2017-02-27 11:52:47 -0600

    added solar noon, removed load data files
    load_Data files is no longer needed since mycroft update

--- SunSkill/__init__.py
+++ SunSkill/__init__.py
@@ -19,7 +19,6 @@ class SunSkill(MycroftSkill):
         super(SunSkill, self).__init__(name="SunSkill")
 
     def initialize(self):
-        self.load_data_files(dirname(__file__))
         sunrise_intent = IntentBuilder("SunRiseIntent"). \
             require("SunRiseKeyword").build()
         sunset_intent = IntentBuilder("SunSetIntent"). \
@@ -28,16 +27,17 @@ class SunSkill(MycroftSkill):
             require("DawnKeyword").build()
         dusk_intent = IntentBuilder("DuskIntent"). \
             require("DuskKeyword").build()
-        
+        noon_intent = IntentBuilder("SolarNoonIntent"). \
+            require("NoonKeyword").build()
 
         self.register_intent(sunrise_intent, self.handle_sunrise_intent)
         self.register_intent(sunset_intent, self.handle_sunset_intent)
         self.register_intent(dawn_intent, self.handle_dawn_intent)
         self.register_intent(dusk_intent, self.handle_dusk_intent)
+        self.register_intent(noon_intent, self.handle_noon_intent)
 
         today = arrow.now().date()
         self.localtz = tzlocal.get_localzone()
-
         
         city = self.config_core.get("location")
         if isinstance(city, str): # city is city name
@@ -48,7 +48,6 @@ class SunSkill(MycroftSkill):
         else: # city is a dict of location information
             lat = city['coordinate']['latitude']
             lon = city['coordinate']['longitude']
-        
 
         sun = SolarTime()
         self.schedule = sun.sun_utc(today, lat, lon)
@@ -69,6 +68,9 @@ class SunSkill(MycroftSkill):
         dusk = self.schedule['dusk'].astimezone(self.localtz)
         self.speak_dialog("dusk", {"dusk": str(dusk)[10:16]})
 
+    def handle_noon_intent(self, message):
+        noon = self.schedule['noon'].astimezone(self.localtz)
+        self.speak_dialog("noon", {"noon": str(noon)[10:16]})
 
     def stop(self):
         pass
